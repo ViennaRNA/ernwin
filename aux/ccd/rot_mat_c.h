c29cb057173992d029a58b5b80a6f21ffa354cc0
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2012-09-12 16:09:31 +0200

    Added rot_mat_c.h

---
+++ aux/ccd/rot_mat_c.h
@@ -0,0 +1,4 @@
+#include <math.h>
+
+void rotation_matrix_c(double *axis, double theta, double *mat);
+void get_closer_rotation_matrix_c(double *TH, double *point, double *M, double *F, double *out_rot_mat);
