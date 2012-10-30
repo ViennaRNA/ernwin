dea617ad0a2466c57c9bc20c3dbfe7b3700b3988
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2012-10-30 19:27:29 +0100

    Debugging the RoughJunctinClosureEnergy.

--- fess/scripts/create_pymol.sh
+++ fess/scripts/create_pymol.sh
@@ -3,11 +3,11 @@
 base=${1##*/}
 pdb=${base%\.*}
 
-OUTPUT_DIR=output/${pdb}
+OUTPUT_DIR=fess/output/${pdb}
 
 OUTPUT_GRAPH_DIR=${OUTPUT_DIR}/graph
 OUTPUT_PYMOL_DIR=${OUTPUT_DIR}/pymol
 
-./scripts/coordinates_to_pymol.py -x $OUTPUT_GRAPH_DIR/temp.comp > $OUTPUT_PYMOL_DIR/coarse_grain.pym 
-./scripts/graph_to_pymol.py $OUTPUT_GRAPH_DIR/temp.comp $OUTPUT_PYMOL_DIR/coarse_grain.pym > $OUTPUT_PYMOL_DIR/cartoon.pml
+./fess/scripts/coordinates_to_pymol.py -x $OUTPUT_GRAPH_DIR/temp.comp > $OUTPUT_PYMOL_DIR/coarse_grain.pym 
+./fess/scripts/graph_to_pymol.py $OUTPUT_GRAPH_DIR/temp.comp $OUTPUT_PYMOL_DIR/coarse_grain.pym > $OUTPUT_PYMOL_DIR/cartoon.pml
 
