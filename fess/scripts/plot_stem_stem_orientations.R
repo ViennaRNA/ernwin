ac3c877dde8b7cb2909271d0a59c0d662dcdcbe6
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2013-02-04 09:08:00 +0100

    stem stem orientation stuff

--- fess/scripts/plot_stem_stem_orientations.R
+++ fess/scripts/plot_stem_stem_orientations.R
@@ -1,23 +1,33 @@
 library(ggplot2)
+install.packages('ggplot2')
 
-t <- read.csv('../stats/stem_stem_orientations.csv', head=F, sep=' ')
+plotorientations <- function(t, ts, filename) {
+  length(t[t$V1 < 20,]$V1)
+  min_dist = 0
+  max_dist = 22
+
+  t1 <- t[t$V1 < max_dist & t$V1 > min_dist,]
+  ts1 <- ts[ts$V1 < max_dist & ts$V1 > min_dist,]
+
+  tnew <- data.frame(val=t1$V3, sampled="N")
+  tsnew <- data.frame(val=ts1$V3, sampled="Y")
+  anew <- rbind(tnew, tsnew)
+
+  length(t1$V3)
+  length(ts1$V3)
+  
+  png(filename)
+  ggplot(anew, aes(x=val, fill=sampled)) + geom_density(alpha=.3)
+  dev.off()
+}
+
 ts <- read.csv('../stats/stem_stem_orientations_sampled.csv', head=F, sep=' ')
 
-length(t[t$V1 < 20,]$V1)
-min_dist = 0
-max_dist = 4
+t <- read.csv('../stats/stem_stem_orientations.csv', head=F, sep=' ')
+plotorientations(t, ts, "new_stem_stem_stats.png")
 
-t1 <- t[t$V1 < max_dist & t$V1 > min_dist,]
-ts1 <- ts[ts$V1 < max_dist & ts$V1 > min_dist,]
-
-tnew <- data.frame(val=t1$V3, sampled="N")
-tsnew <- data.frame(val=ts1$V3, sampled="Y")
-anew <- rbind(tnew, tsnew)
-
-length(t1$V3)
-length(ts1$V3)
-
-ggplot(anew, aes(x=val, fill=sampled)) + geom_density(alpha=.3)
+t <- read.csv('../stats/stem_stem_orientations.csv', head=F, sep=' ')
+plotorientations(t, ts, "old_stem_stem_stats.png")
 
 tnew <- data.frame(val=t1$V1, sampled="N")
 tsnew <- data.frame(val=ts1$V1, sampled="Y")
