ViennaRNA/ernwin

commit 6471689d39d04fc6d89c70d37f685e4203cfd13a

"Fixed a bug with Clash energy detection"

fess/builder/energy.py
@@ -338,7 +338,8 @@ class CombinedEnergy:
 
     def dump_measures(self, base_directory, iteration=None):
         for e in it.chain(self.energies,
-                          self.uncalibrated_energies):
+                          self.uncalibrate('Intersection', set([]))
+d_energies):
             e.dump_measures(base_directory, iteration)
 
     def eval_energy(self, sm, verbose=False, background=True,
@@ -592,7 +593,7 @@ class StemVirtualResClashEnergy(EnergyFunction):
             if s1 == s2:
                 continue
 
-            if len(set.intersection(bg.edges[s1], bg.edges[s2])) >= 0:
+            if len(set.intersection(bg.edges[s1], bg.edges[s2])) > 0:
                 # the stems are connected
                 continue
 

commit e36e1e0cb98d11b955bdef562b5c330681715933

Finally fixed bug introduced in 6471689d39d04fc6d89c70d37f685e4203cfd13a

fess/builder/energy.py
@@ -337,8 +337,8 @@ class CombinedEnergy:
             e.reject_last_measure()
 
     def dump_measures(self, base_directory, iteration=None):
-        for e in it.chain(self.energies,
-                          self.uncalibrate('Intersection', set([])):
+        for e in it.chain(self.energies,                       
+                          self.uncalibrated_energies): 
             e.dump_measures(base_directory, iteration)
 
     def eval_energy(self, sm, verbose=False, background=True,