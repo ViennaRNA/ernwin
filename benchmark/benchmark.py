29d30f412621c31f5883ffbff00a5d5b8d6da221
Bernhard C. Thiel <thiel@tbi.univie.ac.at>
2017-01-31 21:57:39 +0100

    Benchmark using forgi.threedee.model.ensemble

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,7 +6,8 @@ from builtins import (ascii, bytes, chr, dict, filter, hex, input,
 from future.builtins.disabled import (apply, cmp, coerce, execfile,
                              file, long, raw_input, reduce, reload,
                              unicode, xrange, StandardError)
-
+import logging    
+logging.basicConfig(level=logging.INFO)
 import argparse
 import glob
 import os.path as op
@@ -14,6 +15,7 @@ import re
 import numpy as np
 import itertools as it
 import forgi.threedee.model.coarse_grain as ftmc
+import forgi.threedee.model.ensemble as ftme
 from sklearn.cluster import DBSCAN
 from fess.builder.samplingStatisticsNew2 import OutfileParser
 import matplotlib.pyplot as plt
@@ -45,72 +47,26 @@ def get_parser():
 
 parser = get_parser()
 if __name__=="__main__":
+
     args = parser.parse_args()
     
     #Read the out.log file
     data = OutfileParser.parse(op.join(args.ernwin_dir,"out.log"))
 
     # Read all structures
-    trajectory = {}
+    traj = {}
     for fn in glob.iglob(op.join(args.ernwin_dir,"step*.coord")):
         step = int(re.search(r'step(\d*).coord', fn).group(1))
-        trajectory[step] = ftmc.CoarseGrainRNA(fn)
-    
-    nr_to_step = { i: step for i, step in enumerate(sorted(trajectory.keys()))}
-
+        traj[step] = ftmc.CoarseGrainRNA(fn)
     if args.reference:
-        ref_cg = ftmc.CoarseGrainRNA(args.reference)
+        ref = ftmc.CoarseGrainRNA(args.reference)
     else:
-        ref_cg = trajectory[nr_to_step[0]]
-        
-    # Create the pairwise distance matrix using cg-RMSD
-    distance_matrix = np.zeros((len(trajectory), len(trajectory)))*np.nan
-    print("Calculating distance matrix for {} structures".format(len(trajectory)))
-    #Calculate RMSD to precursor
-    for i in range(1, len(trajectory)):
-        j=i-1
-        distance_matrix[i,j] = distance_matrix[j,i] = trajectory[nr_to_step[i]].coords.rmsd_to(trajectory[nr_to_step[j]].coords)
-    #Calculate rest of distance matrix. If two subsequent entries are the same, copy the values only.
-    for i in range(len(trajectory)):
-        if i>0 and distance_matrix[i,i-1] < 10**-10: #(Almost) 0 distance
-            distance_copy_previouse_line(distance_matrix, i)
-            continue
-        for j in range(i+2, len(trajectory)):
-            if distance_matrix[j,j-1] < 10**-10: #(Almost) 0 distance
-                distance_copy_previouse_field(distance_matrix, i, j)
-            else:
-                distance_matrix[i,j] = distance_matrix[j,i] = trajectory[nr_to_step[i]].coords.rmsd_to(trajectory[nr_to_step[j]].coords)
-
-    print("Saving distance matrix")
-    np.savetxt("distance_matrix.txt", distance_matrix)        
-    o_rmsd = []
+        ref = None  
+    trajectory = ftme.Ensemble(traj, ref)
+    trajectory.view_db_clustering()
     
-    for i in range(len(trajectory)):
-        o_rmsd.append(trajectory[nr_to_step[i]].coords.rmsd_to(ref_cg.coords))
-    o_rmsd = np.array(o_rmsd)
-        
-    db = DBSCAN(eps=0.5, min_samples=10, metric="precomputed", n_jobs = 4).fit(distance_matrix)
-    
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    
-    unique_labels = set(labels)
-    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
     
     #Energies only for the steps stored
-    energies = np.array([data["Sampling_Energy"][i] for i in sorted(nr_to_step.values())])
+   # energies = np.array([data["Sampling_Energy"][i] for i in sorted(nr_to_step.values())])
     
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = 'k'
-        class_member_mask = (labels == k)
-        plt.plot( o_rmsd[class_member_mask & core_samples_mask],
-                  energies[class_member_mask & core_samples_mask],
-                  'o', markerfacecolor=col, markeredgecolor='k', markersize=8
-                  )
-        plt.plot( o_rmsd[class_member_mask & ~core_samples_mask],
-                  energies[class_member_mask & ~core_samples_mask],
-                  'o', markerfacecolor=col, markeredgecolor='k', markersize=4
-                )
-    plt.show()
+
