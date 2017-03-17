a725f060edd7b64fcabb16c4bcf9d78289e14b3e
Bernhard Thiel <thiel@tbi.univie.ac.at>
2017-03-17 09:51:57 +0100

    Added missing file utils.py

---
+++ fess/aux/utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import (ascii, bytes, chr, dict, filter, hex, input, #pip install future
+                      int, map, next, oct, open, pow, range, round,
+                      str, super, zip)
+from future.builtins.disabled import (apply, cmp, coerce, execfile,
+                             file, long, raw_input, reduce, reload,
+                             unicode, xrange, StandardError)
+__metaclass__=type
+
+def get_all_subclasses(cls, include_base = False):
+    """
+    Thanks to fletom at http://stackoverflow.com/a/17246726/5069869
+    """        
+    all_subclasses = []
+    if include_base:
+        all_subclasses.append(cls)
+
+    for subclass in cls.__subclasses__():
+        all_subclasses.append(subclass)
+        all_subclasses.extend(get_all_subclasses(subclass))
+    return all_subclasses
