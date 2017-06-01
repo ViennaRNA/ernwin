684a04bc2242bc2514a9f8e2465051973bf969f4
Bernhard Thiel <thiel@tbi.univie.ac.at>
2017-06-01 17:09:55 +0200

    Fixed setup.py and added logging

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from distutils.core import setup
 from distutils.command.build_py import build_py as _build_py
 import subprocess
 import os
-        
-
+import logging
+log=logging.getLogger("setup_py")        
+logging.basicConfig(level=logging.INFO)
+log.info("Now building")
 try: #If we are in a git-repo, get git-describe version.
     path = os.path.abspath(os.path.dirname(__file__))    
-    ernwin_version = subprocess.check_output([os.path.join(path, "git"), "describe", "--always"]).strip()
+    ernwin_version = subprocess.check_output(["git", "describe", "--always"]).strip()
+    log.info("Ernwin version read: %s", ernwin_version)
     try:
-        subprocess.check_call([os.path.join(path, "git"), 'diff-index', '--quiet', 'HEAD', '--'])
+        subprocess.check_call(["git", 'diff-index', '--quiet', 'HEAD', '--'])
+
     except subprocess.CalledProcessError:
         ernwin_version+="+uncommited_changes"
     #Use a subclass of build_py from distutils to costumize the build.
@@ -21,7 +25,8 @@ try: #If we are in a git-repo, get git-describe version.
             try: 
                 outfile = self.get_module_outfile(self.build_lib, ["fess"], "__init__")
                 os.remove(outfile) #If we have an old version, delete it, so _build_py will copy the original version into the build directory.
-            except: 
+            except:
+                log.exception("")
                 pass
             # Superclass build
             print("Running _build_py")
@@ -30,8 +35,10 @@ try: #If we are in a git-repo, get git-describe version.
             outfile = self.get_module_outfile(self.build_lib, ["fess"], "__init__")
             # Apped the version number to init.py
             with open(outfile, "a") as of:
+                log.info("ernwin_version %s", ernwin_version)
                 of.write('\n__complete_version__ = "{}"'.format(ernwin_version))
 except OSError: #Outside of a git repo, do nothing.
+    log.exception("Not in a git repo")
     build_py = _build_py
 
 
