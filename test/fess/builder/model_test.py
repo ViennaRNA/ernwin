b27438dd575736da57642dea4f67ee37da26672d
Bernhard Thiel <thiel@tbi.univie.ac.at>
2018-01-03 08:32:29 +0100

    Patch a test to work with forgi 1.0

--- test/fess/builder/model_test.py
+++ test/fess/builder/model_test.py
@@ -591,7 +591,7 @@ class ReconstructionTests(unittest.TestCase):
         theta_x = math.atan2(rot_mat[2,1], rot_mat[2,2])
         theta_y = math.atan2(-rot_mat[2,0], math.sqrt(rot_mat[2,1]**2+rot_mat[2,2]**2))
         theta_z = math.atan2(rot_mat[1,0], rot_mat[0,0])
-        self.assertTrue(ftuv.is_almost_colinear(np.dot(stem1.vec(), rot_mat), np.array([10,0,0.])))
+        self.assertTrue(ftuv.is_almost_parallel(np.dot(stem1.vec(), rot_mat), np.array([10,0,0.])))
         self.assertAlmostEqual(theta_x, 0)
         self.assertAlmostEqual(theta_y, -math.pi/2)
         self.assertAlmostEqual(theta_z, -math.pi/4)
