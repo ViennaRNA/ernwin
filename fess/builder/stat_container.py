ViennaRNA/ernwin

commit 2680535b73e397e2d43f65f778d84e1a0d006858

Convert MaximumResursionDepth Error to Lookup error in stat_container

fess/builder/stat_container.py
@@ -121,7 +121,16 @@ class StatStorage(object):
         raise StopIteration
 
     @lru_cache(maxsize = 128)
-    def _possible_stats(self, stat_type, key, min_entries = 100, strict=False):
+    def _possible_stats(self, stat_type, key, min_entries=100, strict=False):
+        try:
+            self._possible_stats_inner(stat_type, key, min_entries, strict)
+        except RuntimeError as e:
+            if "recursion" not in str(e):
+                raise
+            # Maximum recursion depth exceeded.
+            raise LookupError("No stats found for {} with key {}, even after reducing length.".format(stat_type, key))
+
+    def _possible_stats_inner(self, stat_type, key, min_entries = 100, strict=False):
         """
         :returns: Two lists, `weights` and `choose_from` of the same length.
                   weights is a list of floats, choose_from is a list of stats.
@@ -158,7 +167,7 @@ class StatStorage(object):
                     if (stat_type, key, min_entries) not in self._has_reported:
                         log.error("Trying key %s instead of %s for %s", key-1, key, stat_type)
                         self._has_reported.add((stat_type, key, min_entries))
-                    return self._possible_stats(stat_type, key-1, min_entries, strict)
+                    return self._possible_stats_inner(stat_type, key-1, min_entries, strict)
                 elif stat_type == "angle" and (key[0]>0 or 1000>key[1]>0):
                     if 1000>key[1]>key[0]:
                         new_key = (key[0], key[1]-1, key[2])
@@ -167,11 +176,9 @@ class StatStorage(object):
                     if (stat_type, key, min_entries) not in self._has_reported:
                         log.error("Trying key %s instead of %s for %s", new_key, key, stat_type)
                         self._has_reported.add((stat_type, key, min_entries))
-                    return self._possible_stats(stat_type, new_key, min_entries, strict)
-
+                    return self._possible_stats_inner(stat_type, new_key, min_entries, strict)
             # If everything else fails, raise an error even if strict was disabled.
             raise LookupError("No stats found for {} with key {}".format(stat_type, key))
-
         return weights, choose_from
 
 

fess/builder/reconstructor.py
@@ -539,6 +539,7 @@ def mend_breakpoints(chains, gap):
                 mended_chains[chain_id] = ftup.get_first_chain(
                                                 op.join(tmpdir,
                                                         "mended_{}.pdb".format(chain_id)))
+                log.info("Mended:", mended_chains)
                 mended_chains[chain_id].id = chain_id
             else:
                 mended_chains[chain_id] = chains[chain_id]

commit 158998bc2aee0203bd9cbfef81faa745f8f3dd8d

Fix a bug introduced in 2680535b73e397e2d43f65f778d84e1a0d006858

fess/builder/stat_container.py
@@ -123,7 +123,7 @@ class StatStorage(object):
     @lru_cache(maxsize = 128)
     def _possible_stats(self, stat_type, key, min_entries=100, strict=False):
         try:
-            self._possible_stats_inner(stat_type, key, min_entries, strict)
+            return self._possible_stats_inner(stat_type, key, min_entries, strict)
         except RuntimeError as e:
             if "recursion" not in str(e):
                 raise