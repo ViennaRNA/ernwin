89e2e2201ef3152dc16e4d041d42155a53650b6e
Bernhard C. Thiel <thiel@tbi.univie.ac.at>
2024-02-12 22:21:30 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 setup(
       name='ernwin',
-      version='1.2.0',
+      version='1.3.0',
       description='Coarse Grain 3D RNA Structure Modelling',
       author='Bernhard Thiel, Peter Kerpedjiev',
       author_email='thiel@tbi.univie.ac.at, pkerp@tbi.univie.ac.at',
